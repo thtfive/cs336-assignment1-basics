50749f657bab48f96d46aebd9c204d78c290bb46
Huahui Zhang <thtfive@163.com>
2025-08-26 12:02:44 +0000

    use hypeparameters from the tutorial

--- cs336_basics/training_together.py
+++ cs336_basics/training_together.py
@@ -4,6 +4,9 @@ import argparse
 import json
 import numpy as np
 from typing import Sequence
+import wandb
+import os
+from datetime import datetime
 
 from cs336_basics.transformer import TransformerLM
 from cs336_basics.log_setup import init_logger, get_logger
@@ -14,8 +17,7 @@ from cs336_basics.cross_entropy import cross_entropy
 from cs336_basics.checkpointing import save_checkpoint, load_checkpoint
 from cs336_basics.data_loader import TrainingDataSet, build_or_load
 from torch.utils.data import Dataset, DataLoader
-import wandb
-import os
+
 
 wandb_api_key = os.environ["WANDB_API_KEY"]
 wandb.login(key=wandb_api_key)
@@ -24,13 +26,16 @@ def parse_args():
     # parse parameters
     parser = argparse.ArgumentParser(description='Train TransformerLM')
 
+    # Experiment name
+    parser = argparse.add_argument("--exp_name", type=str, default="train gpt")
+
     # TransformLM model parameters
     parser.add_argument("--vocab_size", type=int, default=10000)
-    parser.add_argument("--context_length", type=int, default=64)
-    parser.add_argument("--d_model", type=int, default=256)
-    parser.add_argument("--num_layers", type=int, default=12)
-    parser.add_argument("--num_heads", type=int, default=8)
-    parser.add_argument("--d_ff", type=int, default=1024)
+    parser.add_argument("--context_length", type=int, default=256)
+    parser.add_argument("--d_model", type=int, default=512)
+    parser.add_argument("--num_layers", type=int, default=4)
+    parser.add_argument("--num_heads", type=int, default=16)
+    parser.add_argument("--d_ff", type=int, default=1344)
     parser.add_argument("--rope_theta", type=float, default=10000.0)
 
     # Tokenizer parameters
@@ -68,6 +73,18 @@ def save_training_checkpoint(model, optimizer, iteration, checkpoint_path):
     logger.info("Save checkpoint to {path}", path=checkpoint_path)
 
 
+def format_param_count(num_params):
+    """convert params number to human-friendly format"""
+    if num_params >= 1e9:
+        return f"{num_params / 1e9:.2f}B"
+    elif num_params >= 1e6:
+        return f"{num_params / 1e6:.2f}M"
+    elif num_params >= 1e3:
+        return f"{num_params / 1e3:.2f}K"
+    else:
+      return f"{num_params}"
+
+
 def train(params):
     tokenizer = Tokenizer.from_files(
         vocab_filepath=params.vocab_filepath,
@@ -78,17 +95,14 @@ def train(params):
     params.vocab_size = len(tokenizer.vocab)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+    run_name = f"{params.exp_name}-{datetime.now().strftime('%Y%m%d-%H%M%S')}"
     wandb.init(
         project="cs336-project",  # Project name (required, will create or reuse this project in W&B)
-        name="training-gpt",     # Experiment name (optional, random name will be generated if not set)
-        config={                # Record hyperparameters (dictionary format)
-            "learning_rate": params.lr,
-            "architecture": "Transformer+PreNorm+RoPE",
-            "dataset": "tiny-stories",
-            "epochs": 1,
-            "batch_size": params.batch_size,
-        }
+        name=run_name,     # Experiment name (optional, random name will be generated if not set)
+        config=vars(params),                # Record hyperparameters (dictionary format)
     )
+    wandb.finish()
+    wandb.init(mode="disabled")
 
     # load model
     model = TransformerLM(
@@ -102,6 +116,11 @@ def train(params):
     )
     model.to(device)
     wandb.watch(model, log="all", log_freq=10)
+
+    total_params = sum(p.numel() for p in model.parameters())
+    trainable_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    logger.info(f"Total params: {total_params} ({format_param_count(total_params)})")
+    logger.info(f"Total trainable params: {trainable_params}")
 
     # load training data
     train_dataset = build_or_load(
@@ -147,7 +166,6 @@ def train(params):
                 iteration=iteration,
                 checkpoint_path=checkpoint_path
             )
-
 
     # save checkpoint
     checkpoint_path = params.checkpoint_dir + "model_step{step}.pth".format(step=iteration)
