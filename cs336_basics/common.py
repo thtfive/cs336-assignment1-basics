e90c596d669ac3fb8dedb0f4e992ff00831ec686
Huahui Zhang <thtfive@163.com>
2025-08-28 09:06:08 +0000

    add find_chunk_boundaries in common

--- cs336_basics/common.py
+++ cs336_basics/common.py
@@ -1,4 +1,6 @@
 from tests.common import FIXTURES_PATH, gpt2_bytes_to_unicode
+import os
+from typing import BinaryIO
 
 def write_vocab_to_file(vocab, file_path):
     gpt2_byte_decoder = {k: v for k, v in gpt2_bytes_to_unicode().items()}
@@ -46,3 +48,50 @@ def read_merges_from_file(file_path):
         tokens_in_bytes = bytes([gpt2_byte_encoder[ch] for ch in token1]), bytes([gpt2_byte_encoder[ch] for ch in token2])
         merges.append((tokens_in_bytes[0], tokens_in_bytes[1]))
     return merges
+
+
+def find_chunk_boundaries(
+    file: BinaryIO,
+    desired_num_chunks: int,
+    split_special_token: bytes,
+) -> list[int]:
+    """
+    Chunk the file into parts that can be counted independently.
+    May return fewer chunks if the boundaries end up overlapping.
+    """
+    assert isinstance(split_special_token, bytes), "Must represent special token as a bytestring"
+
+    # Get total file size in bytes
+    file.seek(0, os.SEEK_END)
+    file_size = file.tell()
+    file.seek(0)
+
+    chunk_size = file_size // desired_num_chunks
+
+    # Initial guesses for chunk boundary locations, uniformly spaced
+    # Chunks start on previous index, don't include last index
+    chunk_boundaries = [i * chunk_size for i in range(desired_num_chunks + 1)]
+    chunk_boundaries[-1] = file_size
+
+    mini_chunk_size = 4096  # Read ahead by 4k bytes at a time
+
+    for bi in range(1, len(chunk_boundaries) - 1):
+        initial_position = chunk_boundaries[bi]
+        file.seek(initial_position)  # Start at boundary guess
+        while True:
+            mini_chunk = file.read(mini_chunk_size)  # Read a mini chunk
+
+            # If EOF, this boundary should be at the end of the file
+            if mini_chunk == b"":
+                chunk_boundaries[bi] = file_size
+                break
+
+            # Find the special token in the mini chunk
+            found_at = mini_chunk.find(split_special_token)
+            if found_at != -1:
+                chunk_boundaries[bi] = initial_position + found_at
+                break
+            initial_position += mini_chunk_size
+
+    # Make sure all boundaries are unique, but might be fewer than desired_num_chunks
+    return sorted(set(chunk_boundaries))
